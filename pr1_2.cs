0ffc7d1b56d8e4458bea211a5fa97283315d1912
KaHiSTpA <324537403+KaHiSTpA@users.noreply.github.com>
2026-09-17 14:56:13 +0900

    add pr1_2

---
+++ pr1_2.cs
@@ -0,0 +1,10 @@
+// // Выполнил Лизунов Михаил ИСИП-25
+
+// // 2
+// Console.Write("Введите сумму в рублях: ");
+// decimal rubles = Convert.ToDecimal(Console.ReadLine());
+
+// decimal usd_rate = 84.25m;
+// decimal dollars = rubles / usd_rate;
+
+// Console.WriteLine($"Сумма в долларах: {dollars:F2}");
