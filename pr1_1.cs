bf99c8179cad55b7425f2b4fe194a49048cfeb7f
KaHiSTpA <324537403+KaHiSTpA@users.noreply.github.com>
2026-09-17 14:23:24 +0900

    add pr1_1

---
+++ pr1_1.cs
@@ -0,0 +1,13 @@
+// // Выполнил Лизунов Михаил ИСИП-25
+
+// // 1
+// Console.Write("Введите длину прямоугольника: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите ширину прямоугольника: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// int s = x * y;
+// int p = x * 2 + y * 2;
+
+// Console.WriteLine($"Площадь прямоугольника: {s}, периметр прямоугольника: {p}");
